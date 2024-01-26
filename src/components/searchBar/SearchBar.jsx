36cfb8ca61a022311158db5a80b3db37190afd4e
JayroGT <jayro.esp.sg@gmail.com>
2024-01-26 22:54:14 +0800

    Add: create searchBar

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import style from './SearchBar.module.css';
 
 
-export const SearchBar = () => {
+export const SearchBar = (onSearch, setCountryFilter) => {
 
     // const [item, setItem] = useState('');
 	// const formatItem = item.charAt(0).toUpperCase() + item.slice(1).toLowerCase()
@@ -11,6 +11,22 @@ export const SearchBar = () => {
 	// 	setCountryFilter(formatItem)
 	// },[setCountryFilter, formatItem])
 
+    // useEffect(() => {
+    //     dispatch(filter({ filterType: "**", filterValue: *** }));
+    //     if (**.length >= 30) {
+    //       dispatch(get**(**)).then((res) => {
+    //         setSearchResult(res.payload);
+    //       });
+    //     }
+    //     dispatch(orderName("Ascendente"));
+    //   }, [dispatch, ]);
+
+
+      function handleSearch(name) {
+        dispatch(getCountryByName(name)).then((res) => {
+          setSearchResult(res.payload);
+        });
+      }
   return (
     <div 
     className={style.content}
