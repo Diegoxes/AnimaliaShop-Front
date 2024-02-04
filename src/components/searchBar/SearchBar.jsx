c7f4d3b239a2e7cdb2764ee47b1b40969205f392
LuisCode1 <64246172+b4dow@users.noreply.github.com>
2024-02-04 10:06:01 +0100

    Añadiendo cambios de la navbar y el carrito

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -19,31 +19,42 @@ export const SearchBar = () => {
     }
   };
   return (
-<div className="mx-2 my-10 rounded-xl border bg-white px-4 py-5 max-w-lg mx-auto">
-      <form onSubmit={handleSearch} className="mb-2 flex">
-        <div className="mr-1 w-full">
-        <input
-          type='text'
-          value={productId}
-          name='search'
-          onChange={handleChange}
-          id='search'
-          placeholder='Buscar producto'
-          className="w-full px-3 py-2 border rounded-md focus:ring focus:border-blue-300"
-        />
+    <div className='mx-2 my-10 rounded-xl border bg-white px-4 py-5 max-w-lg mx-auto'>
+      <form onSubmit={handleSearch} className='mb-2 flex'>
+        <div className='mr-1 w-full'>
+          <input
+            type='text'
+            value={productId}
+            name='search'
+            onChange={handleChange}
+            id='search'
+            placeholder='Buscar producto'
+            className='w-full px-3 py-2 border rounded-md focus:ring focus:border-blue-300'
+          />
         </div>
-        <button type='submit' className="shrink-0 flex h-12 w-12 items-center justify-center rounded-lg bg-blue-600 text-white hover:bg-blue-700">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"></path>
-        </svg>
+        <button
+          type='submit'
+          className='shrink-0 flex h-12 w-12 items-center justify-center rounded-lg bg-blue-600 text-white hover:bg-blue-700'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            strokeWidth='1.5'
+            stroke='currentColor'
+            className='h-6 w-6'>
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'></path>
+          </svg>
         </button>
       </form>
     </div>
   );
 };
 
-
-{/* 
+{
+  /* 
 <div class="mx-2 my-10 rounded-xl border bg-white px-4 py-8">
   <div class="mb-2 flex">
     <div class="mr-1 w-full">
@@ -58,4 +69,5 @@ export const SearchBar = () => {
       </svg>
     </button>
   </div>
-</div> */}
+</div> */
+}
